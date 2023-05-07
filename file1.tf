10735081763476eeb82a5bb992f82d5b8e154561
bcdebug <126670639+bcdebug@users.noreply.github.com>
2023-05-07 13:59:00 +0300

    test commit

--- file1.tf
+++ file1.tf
@@ -1,8 +1,1 @@
-resource "aws_lb_listener" "https1" {
-  load_balancer_arn = ""
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  default_action {
-    type = ""
-  }
-}
+my updated file contents
